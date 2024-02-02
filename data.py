610983bd70bbaa95c803dbb69c8d5ffbcc1a1e84
MaherJamil <maherj@nycstudents.net>
2024-02-02 12:27:02 -0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,34 @@
+""" #Data Types
+#Numbers 1,2,3
+def add(x,y):
+    print(x + y)
+add(1,2)
+#strings "a,b,c"
+name = "Mark"
+def greeting(person):
+    print(person)
+greeting(name)
+#1 and "1" are not the same
+add("1","2")
+#undefined/null
+
+#booLeans
+tenure = True
+def is_tenured(status):
+    if(status == True):
+        print("They have tenure")
+    else:
+        print("they are not tenured") """
+
+""" x=3
+y=float(3)
+print(x,y) """
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+
+print(values[0])
+print(values[6]) """
+
